b71909aac62685039105eca78cbef87633ce5272
Tim Price <tim.price@stfc.ac.uk>
2016-07-06 09:20:04 +0100

    Added references to license for hardware code

--- Hardware/camera/cameraController.cpp
+++ Hardware/camera/cameraController.cpp
@@ -1,3 +1,17 @@
+//              This file is part of VELA-CLARA-Controllers.                          //
+//------------------------------------------------------------------------------------//
+//    VELA-CLARA-Controllers is free software: you can redistribute it and/or modify  //
+//    it under the terms of the GNU General Public License as published by            //
+//    the Free Software Foundation, either version 3 of the License, or               //
+//    (at your option) any later version.                                             //
+//    VELA-CLARA-Controllers is distributed in the hope that it will be useful,       //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of                  //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   //
+//    GNU General Public License for more details.                                    //
+//                                                                                    //
+//    You should have received a copy of the GNU General Public License               //
+//    along with VELA-CLARA-Controllers.  If not, see <http://www.gnu.org/licenses/>. //
+
 #include "cameraController.h"
 // stl
 cameraController::cameraController( const std::string configFileLocation, const std::string configIOCFileLocation, const  bool show_messages, const bool show_debug_messages )
--- Hardware/magnet/magnetController.h
+++ Hardware/magnet/magnetController.h
@@ -1,3 +1,17 @@
+//              This file is part of VELA-CLARA-Controllers.                          //
+//------------------------------------------------------------------------------------//
+//    VELA-CLARA-Controllers is free software: you can redistribute it and/or modify  //
+//    it under the terms of the GNU General Public License as published by            //
+//    the Free Software Foundation, either version 3 of the License, or               //
+//    (at your option) any later version.                                             //
+//    VELA-CLARA-Controllers is distributed in the hope that it will be useful,       //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of                  //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   //
+//    GNU General Public License for more details.                                    //
+//                                                                                    //
+//    You should have received a copy of the GNU General Public License               //
+//    along with VELA-CLARA-Controllers.  If not, see <http://www.gnu.org/licenses/>. //
+
 #ifndef VELA_MAG_CONTROLLER_H_
 #define VELA_MAG_CONTROLLER_H_
 // stl
@@ -112,4 +126,3 @@ class magnetController : public controller
 
 
 #endif // VELA_MAG_CONTROLLER_H_
-
