5b45b3874c7175c25691bc24bd86a53f7eb5704a
Tim Price <tim.price@stfc.ac.uk>
2016-07-06 08:52:52 +0100

    Reference to License added to some source files

--- Hardware/interface.cpp
+++ Hardware/interface.cpp
@@ -1,3 +1,17 @@
+//              This file is part of VELA-CLARA-Controllers.                          //
+//------------------------------------------------------------------------------------//
+//    VELA-CLARA-Controllers is free software: you can redistribute it and/or modify  //
+//    it under the terms of the GNU General Public License as published by            //
+//    the Free Software Foundation, either version 3 of the License, or               //
+//    (at your option) any later version.                                             //
+//    VELA-CLARA-Controllers is distributed in the hope that it will be useful,       //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of                  //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   //
+//    GNU General Public License for more details.                                    //
+//                                                                                    //
+//    You should have received a copy of the GNU General Public License               //
+//    along with VELA-CLARA-Controllers.  If not, see <http://www.gnu.org/licenses/>. //
+
 #include "interface.h"
 // stl
 #include <iostream>
@@ -280,4 +294,3 @@ void interface::updateTime( const epicsTimeStamp & stamp, double & val, std::str
     //std::cout << std::setprecision(15) << std::showpoint<<   val << std::endl;
     str = timeString;
 }
-
