258b984af96cdc5029c371be72f197e8079bb0a9
Tim Price <tim.price@stfc.ac.uk>
2016-09-05 14:43:58 +0100

    Updated test.py script for magnet

--- Controllers/VELA/INJECTOR/velaINJMagnets/bin/Release/test.py
+++ Controllers/VELA/INJECTOR/velaINJMagnets/bin/Release/test.py
@@ -1,12 +1,12 @@
 import sys,os
 import numpy as np
-os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
-os.environ["EPICS_CA_ADDR_LIST"] = "192.168.82.10"
-os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000000"
+#os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
+#os.environ["EPICS_CA_ADDR_LIST"] = "192.168.82.10"
+#os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000000"
 
 import velaINJMagnetControl as VIMC
 
-a = VIMC.velaINJMagnetController(True,False,'C:\Users\wln24624\Documents\VELA-CLARA-Controllers')
+a = VIMC.velaINJMagnetController(True,False)
 
 
 print( np.array(a.getQuadNames()))
