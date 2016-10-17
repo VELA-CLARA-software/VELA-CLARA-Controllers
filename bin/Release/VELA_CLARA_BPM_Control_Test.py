4466b1188b30c92b6d42cd077c35d14e07fedde0
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2016-10-17 12:07:10 +0100

    Modifications to BPM controllers
    This conforms to the new structure, in which the config path for a given part of the machine (real or virtual) is read in via Python - see python test file in "bin" folder.

---
+++ bin/Release/VELA_CLARA_BPM_Control_Test.py
@@ -0,0 +1,34 @@
+import sys,os
+import time
+import randomPVs
+os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
+os.environ["EPICS_CA_ADDR_LIST"] = "10.10.0.12"
+os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000"
+
+#sys.path.append('D:\\VELA-CLARA-Controllers-master\\VELA-CLARA-Controllers\\Controllers\\VELA\\INJECTOR\\velaINJBeamPositionMonitors\\bin\\Release')
+
+import VELA_CLARA_BPM_Control as vbpmc
+import epics
+
+def main():
+	srPV = randomPVs.setRandomPV()
+	s=vbpmc.init()
+	p=s.virtual_VELA_INJ_BPM_Controller()
+	numShots = 10
+	pv1 = 'VM-EBT-INJ-DIA-BPMC-02:DATA:B2V.VALA'
+	srPV.setPV(pv1,1,10,10,10, "array")
+	p.monitorDataForNShots(numShots,'BPM01')
+	while p.isMonitoringBPMData('BPM01'):
+		time.sleep(0.01)
+	q = p.getAllBPMData('BPM01')
+	print len(q.x)
+	print 'BPM 01 Data'
+	print 'name = ', q.name
+	for i in range(0,numShots):
+		print 'x = ', q.x[i]
+		print 'y   = ', q.y[i]
+		print 'q   = ', q.q[i]
+		print 'pu1   = ', q.pu1[i]
+		
+if __name__ == "__main__":
+	main()
