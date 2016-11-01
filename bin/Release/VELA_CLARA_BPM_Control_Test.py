c475673233408e688fc552984b5fa8bd2a2e46a3
adb-xkc85723 <alexander.brynes@stfc.ac.uk>
2016-11-01 11:39:20 +0000

    Added DLLs and example files for BPMs/scopes/valves.

--- bin/Release/VELA_CLARA_BPM_Control_Test.py
+++ bin/Release/VELA_CLARA_BPM_Control_Test.py
@@ -1,6 +1,5 @@
 import sys,os
 import time
-import randomPVs
 os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
 os.environ["EPICS_CA_ADDR_LIST"] = "10.10.0.12"
 os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000"
@@ -11,24 +10,31 @@ import VELA_CLARA_BPM_Control as vbpmc
 import epics
 
 def main():
-	srPV = randomPVs.setRandomPV()
 	s=vbpmc.init()
 	p=s.virtual_VELA_INJ_BPM_Controller()
 	numShots = 10
-	pv1 = 'VM-EBT-INJ-DIA-BPMC-02:DATA:B2V.VALA'
-	srPV.setPV(pv1,1,10,10,10, "array")
-	p.monitorDataForNShots(numShots,'BPM01')
-	while p.isMonitoringBPMData('BPM01'):
-		time.sleep(0.01)
-	q = p.getAllBPMData('BPM01')
-	print len(q.x)
-	print 'BPM 01 Data'
-	print 'name = ', q.name
-	for i in range(0,numShots):
-		print 'x = ', q.x[i]
-		print 'y   = ', q.y[i]
-		print 'q   = ', q.q[i]
-		print 'pu1   = ', q.pu1[i]
+	bpmlist = vbpmc.std_vector_string()
+	bpmlist.append('BPM02')
+	bpmlist.append('BPM01')
+	print bpmlist
+	p.monitorDataForNShots(numShots,'BPM02')
+	for i in bpmlist:
+		while p.isMonitoringBPMData(i):
+			time.sleep(0.01)
+	bpm2x = p.getBPMXVec('BPM02')
+	for i in range(numShots)
+		print bpm2x[i]
+	while True:
+		q = p.getAllBPMData('BPM02')
+		print len(q.x)
+		print 'BPM 02 Data'
+		print 'name = ', q.name
+		print 'x         = ', q.x[0]
+		print 'y         = ', q.y[0]
+		print 'q         = ', q.q[0]
+		print 'pu1       = ', q.pu1[0]
+		print 'timestamp = ', q.timeStamps[0]
+		time.sleep(0.1)
 		
 if __name__ == "__main__":
 	main()
